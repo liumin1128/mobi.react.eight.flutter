c01d473a8648f3229f8454b96d992d03b610e60a
liumin1128 <970568830@qq.com>
2019-07-26 02:54:49 +0800

    更新

--- lib/pages/home/index.dart
+++ lib/pages/home/index.dart
@@ -1,4 +1,6 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import '../dynamic/list/main.dart';
 import '../news/list/index.dart';
 
@@ -11,8 +13,9 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
-  int _selectedIndex = 1;
+  // int _selectedIndex = 1;
   static const TextStyle optionStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  
   static List<Widget> _widgetOptions = <Widget>[
     DynamicList(),
     NewsList(),
@@ -20,38 +23,51 @@ class _HomePageState extends State<HomePage> {
       'Index 1: Business',
       style: optionStyle,
     ),
-    Text(
-      'Index 2: School',
-      style: optionStyle,
-    ),
   ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // appBar: AppBar(
-      //   title: const Text('BottomNavigationBar Sample'),
-      // ),
-      body: Center(
-        child: _widgetOptions[_selectedIndex],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(icon: Icon(Icons.explore), title: Text('动态')),
-          BottomNavigationBarItem(icon: Icon(Icons.business),title: Text('资讯')),
-          BottomNavigationBarItem(icon: Icon(Icons.account_circle),title: Text('我的')),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped,
-      ),
+
+    return CupertinoTabScaffold(
+        tabBar: CupertinoTabBar(
+          items: [
+            BottomNavigationBarItem(icon: Icon(CupertinoIcons.home,),title: Text("动态")),
+            BottomNavigationBarItem(icon: Icon(CupertinoIcons.news,),title: Text("资讯")),
+            BottomNavigationBarItem(icon: Icon(CupertinoIcons.person,),title: Text("设置")),
+          ],
+          activeColor: Theme.of(context).accentColor,
+          // inactiveColor: Color(0xff333333),
+          // backgroundColor: Color(0xfff1f1f1),
+          // iconSize: 25.0,
+        ),
+        tabBuilder: (context, index) {
+          return CupertinoTabView(
+            builder: (context) {
+              return _widgetOptions[index];
+            },
+          );
+        }
     );
+
+
+    // return Scaffold(
+    //   // appBar: AppBar(
+    //   //   title: const Text('BottomNavigationBar Sample'),
+    //   // ),
+    //   body: Center(
+    //     child: _widgetOptions[_selectedIndex],
+    //   ),
+    //   bottomNavigationBar: BottomNavigationBar(
+    //     items: const <BottomNavigationBarItem>[
+    //       BottomNavigationBarItem(icon: Icon(Icons.explore), title: Text('动态')),
+    //       BottomNavigationBarItem(icon: Icon(Icons.business),title: Text('资讯')),
+    //       BottomNavigationBarItem(icon: Icon(Icons.account_circle),title: Text('我的')),
+    //     ],
+    //     currentIndex: _selectedIndex,
+    //     selectedItemColor: Colors.amber[800],
+    //     onTap: _onItemTapped,
+    //   ),
+    // );
   }
 }
 
