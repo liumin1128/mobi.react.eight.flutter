271fcf11f7172dc857ee041903c8010c6ff27b21
liumin1128 <970568830@qq.com>
2019-07-26 03:03:10 +0800

    更新

---
+++ lib/pages/news/list/index copy.dart
@@ -0,0 +1,240 @@
+import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+import '../../../components/ListViewPro/index.dart';
+import '../../../components/Graphql/Query/index.dart';
+import '../../../graphql/schema/news.dart';
+import '../../../utils/common.dart';
+
+class NewsList extends StatefulWidget {
+
+  NewsList({ Key key, this.title }) : super(key: key);
+  final String title;
+
+  @override
+  _NewsListState createState() => _NewsListState();
+}
+
+class _NewsListState extends State<NewsList> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('动态'),
+      ),
+      body: QueryPro(
+        builder: (list, loading, { VoidCallback refetch, VoidCallback fetchMore }) {
+
+          if(list.length == 0 && loading) return Text('Loading');
+
+          print('list');
+          print(list);
+          print(list.length);
+
+          return Container(
+            child: ListViewPro(
+              onRefresh: refetch,
+              onScrollToBottom: fetchMore,
+              itemCount: list.length + 1,
+              itemBuilder: (_, int index) {
+
+                if(index == list.length) return Container(
+                  padding: const EdgeInsets.only(top: 16,bottom: 16),
+                  child: Center(
+                    child: Container(
+                      width: 16,
+                      height: 16,
+                      child: CircularProgressIndicator(
+                        strokeWidth: 2,
+                      )
+                    )
+                  )
+                );
+
+                var data = list[index];
+                var cover = data['cover'] == null ? data['photos'][0] : data['cover'];
+                var smallCover = cover == null ? 'https://imgs.react.mobi/FldU5XAVJksEDNDEs7MZiF36DMAz' : getSmallImg(cover, 160, 160);
+                var createdAt = RelativeDateFormat.format(new DateTime.fromMicrosecondsSinceEpoch(data['createdAt']));
+
+                return Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 16,vertical: 8),
+                  child: Row(
+                    // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Image.network(smallCover, width: 80, height: 80, fit: BoxFit.cover),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(16,0,0,0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(data['title'], 
+                                softWrap: true,
+                                overflow: TextOverflow.ellipsis, 
+                                maxLines: 2,
+                                style: const TextStyle(
+                                  fontSize: 18.0,
+                                  color: Colors.black87,
+                                )
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                  Text(data['appName'], style: const TextStyle(fontSize: 14.0, color: Colors.black38)),
+                                  Text(createdAt, style: const TextStyle(fontSize: 14.0, color: Colors.black38)),
+                                ],
+                              )
+                          ],)
+                        )
+                      )
+                    ]
+                  )
+                );
+                // return new ListTile(
+                //   thumbnail: Image.network(smallCover, width: 100, height: 100, fit: BoxFit.cover),
+                //   title: Text(data['title']),
+                //   subtitle: Row(
+                //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                //     children: <Widget>[
+                //       Text(data['_id']),
+                //     ],
+                //   )
+                // );
+              }
+            )
+          );
+
+        }
+      )
+      
+      
+   
+    );
+  }
+
+  //  Future<Null> _getMore() async {
+  //   print('_getMore');
+  //   final QueryResult r = await graphQLClientClient.mutate(
+  //     MutationOptions(
+  //       document: uploadMutation,
+  //       variables: {
+  //         'files': [File(filePath)],
+  //       },
+  //     )
+  //   );
+  // }
+
+  
+}
+
+
+  //  GraphQLConsumer(
+  //       builder: (GraphQLClient client) {
+  //         return Container(
+  //           child: Center(
+  //             child: Query(
+  //               options: QueryOptions(
+  //                 document: newsSchema, // this is the query string you just created
+  //                 variables: {
+  //                   'first': 32
+  //                   // 'nRepositories': 50,
+  //                 },
+  //                 // fetchPolicy: 
+  //                 pollInterval: 10,
+  //               ),
+                
+  //               // Just like in apollo refetch() could be used to manually trigger a refetch
+  //               builder: (QueryResult result, { VoidCallback refetch, VoidCallback fetchMore  }) {
+
+  //                 if (result.errors != null) {
+  //                   return Text(result.errors.toString());
+  //                 }
+
+  //                 if (result.loading) {
+  //                   return Text('Loading');
+  //                 }
+
+  //                 List dynamics = result.data['list'];
+
+  //                 Future<Null> _onRefresh() async {
+  //                   print('onRefresh');
+  //                   refetch();
+  //                 }
+
+  //                 Future<Null> _onScrollToBottom() async {
+  //                   print('_onScrollToBottom');
+  //                   final QueryResult res = await client.mutate(
+  //                     MutationOptions(
+  //                       document: newsSchema,
+  //                       variables: {
+  //                         'skip': 1 * 16,
+  //                         'first': 32
+  //                       },
+  //                     ),
+  //                   );
+
+  //                   if(res.hasErrors) return;
+
+  //                   // final List<dynamic> updated = List<dynamic>.from(result.data['list'])..addAll(res.data['list']);
+  //                   final List<dynamic> updated = res.data['list'];
+
+  //                   // print('updated');
+  //                   // print(updated);
+  //                   // print(updated[0]);
+  //                   // print(typenameDataIdFromObject2(updated[0]));
+  //                   // print(updated[0]['title']);
+
+  //                   client.cache.write(typenameDataIdFromObject2(updated[0]), updated[0]);
+  //                   client.cache.write(typenameDataIdFromObject2(updated[1]), updated[1]);
+  //                   client.cache.write(typenameDataIdFromObject2(updated[2]), updated[2]);
+  //                   client.cache.write(typenameDataIdFromObject2(updated[3]), updated[3]);
+
+  //                   // client.cache.restore();
+  //                   // client.queryManager.addQueryResult('NewsList', res); 
+  //                   // client.queryManager.rebroadcastQueries();
+                    
+
+
+
+  //                 }
+
+  //                 print('dynamics');
+  //                 print(dynamics.length);
+
+  //                 return ListViewPro(
+  //                   onRefresh: _onRefresh,
+  //                   onScrollToBottom: _onScrollToBottom,
+  //                   itemCount: dynamics.length,
+  //                   itemBuilder: (_, int index) {
+
+
+  //                     var data = dynamics[index];
+  //                     var cover = data['cover'] == null ? (data['photos'][0] != null ? data['photos'][0] : 'https://imgs.react.mobi/FldU5XAVJksEDNDEs7MZiF36DMAz') : data['cover'];
+  //                     // var createdAt = RelativeDateFormat.format(new DateTime.fromMicrosecondsSinceEpoch(data['createdAt']));
+
+  //                     return new ListTile(
+  //                       leading: Image.network(cover, width: 100, height: 100, fit: BoxFit.cover),
+  //                       title: Text(data['title']),
+  //                       subtitle: Row(
+  //                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+  //                         children: <Widget>[
+  //                           Text(data['_id']),
+  //                         ],
+  //                       )
+  //                     );
+
+
+  //                   }
+  //                 );
+
+
+
+  //               },
+  //             ),
+  //           )
+  //         );
+  //       },
+  //     )
+
+      
--- lib/pages/news/list/index.dart
+++ lib/pages/news/list/index.dart
@@ -5,15 +5,6 @@ import '../../../components/Graphql/Query/index.dart';
 import '../../../graphql/schema/news.dart';
 import '../../../utils/common.dart';
 
- String typenameDataIdFromObject2(Object object) {
-  if (object is Map<String, Object> &&
-      object.containsKey('__typename') &&
-      object.containsKey('_id')) {
-    return "${object['__typename']}/${object['_id']}";
-  }
-  return null;
-}
-
 class NewsList extends StatefulWidget {
 
   NewsList({ Key key, this.title }) : super(key: key);
@@ -35,10 +26,6 @@ class _NewsListState extends State<NewsList> {
         builder: (list, loading, { VoidCallback refetch, VoidCallback fetchMore }) {
 
           if(list.length == 0 && loading) return Text('Loading');
-
-          print('list');
-          print(list);
-          print(list.length);
 
           return Container(
             child: ListViewPro(
@@ -100,16 +87,7 @@ class _NewsListState extends State<NewsList> {
                     ]
                   )
                 );
-                // return new ListTile(
-                //   thumbnail: Image.network(smallCover, width: 100, height: 100, fit: BoxFit.cover),
-                //   title: Text(data['title']),
-                //   subtitle: Row(
-                //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                //     children: <Widget>[
-                //       Text(data['_id']),
-                //     ],
-                //   )
-                // );
+ 
               }
             )
           );
@@ -122,128 +100,4 @@ class _NewsListState extends State<NewsList> {
     );
   }
 
-  //  Future<Null> _getMore() async {
-  //   print('_getMore');
-  //   final QueryResult r = await graphQLClientClient.mutate(
-  //     MutationOptions(
-  //       document: uploadMutation,
-  //       variables: {
-  //         'files': [File(filePath)],
-  //       },
-  //     )
-  //   );
-  // }
-
-  
 }
-
-
-  //  GraphQLConsumer(
-  //       builder: (GraphQLClient client) {
-  //         return Container(
-  //           child: Center(
-  //             child: Query(
-  //               options: QueryOptions(
-  //                 document: newsSchema, // this is the query string you just created
-  //                 variables: {
-  //                   'first': 32
-  //                   // 'nRepositories': 50,
-  //                 },
-  //                 // fetchPolicy: 
-  //                 pollInterval: 10,
-  //               ),
-                
-  //               // Just like in apollo refetch() could be used to manually trigger a refetch
-  //               builder: (QueryResult result, { VoidCallback refetch, VoidCallback fetchMore  }) {
-
-  //                 if (result.errors != null) {
-  //                   return Text(result.errors.toString());
-  //                 }
-
-  //                 if (result.loading) {
-  //                   return Text('Loading');
-  //                 }
-
-  //                 List dynamics = result.data['list'];
-
-  //                 Future<Null> _onRefresh() async {
-  //                   print('onRefresh');
-  //                   refetch();
-  //                 }
-
-  //                 Future<Null> _onScrollToBottom() async {
-  //                   print('_onScrollToBottom');
-  //                   final QueryResult res = await client.mutate(
-  //                     MutationOptions(
-  //                       document: newsSchema,
-  //                       variables: {
-  //                         'skip': 1 * 16,
-  //                         'first': 32
-  //                       },
-  //                     ),
-  //                   );
-
-  //                   if(res.hasErrors) return;
-
-  //                   // final List<dynamic> updated = List<dynamic>.from(result.data['list'])..addAll(res.data['list']);
-  //                   final List<dynamic> updated = res.data['list'];
-
-  //                   // print('updated');
-  //                   // print(updated);
-  //                   // print(updated[0]);
-  //                   // print(typenameDataIdFromObject2(updated[0]));
-  //                   // print(updated[0]['title']);
-
-  //                   client.cache.write(typenameDataIdFromObject2(updated[0]), updated[0]);
-  //                   client.cache.write(typenameDataIdFromObject2(updated[1]), updated[1]);
-  //                   client.cache.write(typenameDataIdFromObject2(updated[2]), updated[2]);
-  //                   client.cache.write(typenameDataIdFromObject2(updated[3]), updated[3]);
-
-  //                   // client.cache.restore();
-  //                   // client.queryManager.addQueryResult('NewsList', res); 
-  //                   // client.queryManager.rebroadcastQueries();
-                    
-
-
-
-  //                 }
-
-  //                 print('dynamics');
-  //                 print(dynamics.length);
-
-  //                 return ListViewPro(
-  //                   onRefresh: _onRefresh,
-  //                   onScrollToBottom: _onScrollToBottom,
-  //                   itemCount: dynamics.length,
-  //                   itemBuilder: (_, int index) {
-
-
-  //                     var data = dynamics[index];
-  //                     var cover = data['cover'] == null ? (data['photos'][0] != null ? data['photos'][0] : 'https://imgs.react.mobi/FldU5XAVJksEDNDEs7MZiF36DMAz') : data['cover'];
-  //                     // var createdAt = RelativeDateFormat.format(new DateTime.fromMicrosecondsSinceEpoch(data['createdAt']));
-
-  //                     return new ListTile(
-  //                       leading: Image.network(cover, width: 100, height: 100, fit: BoxFit.cover),
-  //                       title: Text(data['title']),
-  //                       subtitle: Row(
-  //                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-  //                         children: <Widget>[
-  //                           Text(data['_id']),
-  //                         ],
-  //                       )
-  //                     );
-
-
-  //                   }
-  //                 );
-
-
-
-  //               },
-  //             ),
-  //           )
-  //         );
-  //       },
-  //     )
-
-      
