e2f4ce4c78035ccee88fc96f622d0c70b5dfccdd
liumin1128 <970568830@qq.com>
2019-07-26 14:20:43 +0800

    更新

--- lib/pages/news/list/index.dart
+++ lib/pages/news/list/index.dart
@@ -67,7 +67,17 @@ class _NewsListState extends State<NewsList> {
                     ),
                   );
                 },
+                
                 child: Container(
+                  decoration: new BoxDecoration(
+                    // border: new Border.all(width: 2.0, color: Colors.red),
+                    // color: Colors.grey,
+                    // borderRadius: new BorderRadius.all(new Radius.circular(20.0)),
+                    // image: new DecorationImage(
+                    //   image: new NetworkImage('http://h.hiphotos.baidu.com/zhidao/wh%3D450%2C600/sign=0d023672312ac65c67506e77cec29e27/9f2f070828381f30dea167bbad014c086e06f06c.jpg'),
+                    //   centerSlice: new Rect.fromLTRB(270.0, 180.0, 1360.0, 730.0),
+                    // ),
+                  ),
                   padding: const EdgeInsets.symmetric(horizontal: 16,vertical: 8),
                   child: Row(
                     // mainAxisAlignment: MainAxisAlignment.spaceBetween,
