28551ac2bb66c8f63173e54346712862349a90a1
liumin1128 <970568830@qq.com>
2019-07-28 03:28:06 +0800

    更新

--- lib/pages/user/me/index.dart
+++ lib/pages/user/me/index.dart
@@ -13,9 +13,45 @@ class _UserMeState extends State<UserMe> {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      child: Center(
-        child: Text('xxxxxxxxx'),
-      ),
+
+      child: CustomScrollView(
+        physics: ScrollPhysics(),
+        slivers: <Widget>[
+
+          CupertinoSliverNavigationBar(
+            largeTitle: Text('xxxxxx'),
+            border: Border(
+              top: BorderSide(
+                style: BorderStyle.none,
+              ),
+            )
+          ),
+
+          SliverSafeArea(
+            // top: true,
+            bottom: true,
+            sliver: SliverToBoxAdapter(
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 16),
+                // child: Text('loading more...', textAlign: TextAlign.center)
+                child: Center(
+                  child: Container(
+                    width: 16,
+                    height: 16,
+                    child: CupertinoActivityIndicator(
+                      // strokeWidth: 2,
+                    )
+                  )
+                )
+              )
+            ),
+          )
+              
+        ],
+      )
+
+        
+ 
     );
   }
 }
