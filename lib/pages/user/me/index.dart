fed12a61caebe4d9955851f45ed2eb908812ff42
liumin1128 <970568830@qq.com>
2019-08-24 07:27:58 +0800

    更新

--- lib/pages/user/me/index.dart
+++ lib/pages/user/me/index.dart
@@ -1,24 +1,52 @@
 import 'package:flutter/cupertino.dart';
-import 'package:fish_redux/fish_redux.dart';
-import 'package:flutter/material.dart' hide Action;
-import 'page.dart';
+import 'package:reactmobi/blocs/counter_bloc.dart';
+import 'package:reactmobi/blocs/theme_bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class UserMe extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    println('main');
-    final AbstractRoutes routes = PageRoutes(
-      pages: <String, Page<Object, dynamic>>{
-        'main': MainPage(),
-      },
-    );
-    return CupertinoApp(
-      title: 'Simple',
-      debugShowCheckedModeBanner: false,
-      theme: CupertinoThemeData(
-        primaryColor: Colors.blue,
+    final counterBloc = BlocProvider.of<CounterBloc>(context);
+    final themeBloc = BlocProvider.of<ThemeBloc>(context);
+
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text("用户"),
+        border: Border(
+          top: BorderSide(
+            style: BorderStyle.none,
+          ),
+        ),
+        leading: GestureDetector(
+          onTap: () {
+            counterBloc.dispatch(CounterEvent.increment);
+          },
+          child: Icon(CupertinoIcons.bell),
+        ),
+        trailing: GestureDetector(
+          onTap: () {
+            themeBloc.dispatch(SetTheme(theme: 'dark1'));
+          },
+          child: Icon(CupertinoIcons.bell),
+        ),
+        // trailing: Icon(CupertinoCupertinoIcons.add)
+        // backgroundColor: Colors.white,
       ),
-      home: routes.buildPage('main', null),
+      // child: Center(
+      //   child: Text('1111'),
+      // ),
+      child: Center(
+        child: BlocBuilder<CounterBloc, int>(
+          builder: (context, count) {
+            return Center(
+              child: Text(
+                '$count',
+                style: TextStyle(fontSize: 24.0),
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
