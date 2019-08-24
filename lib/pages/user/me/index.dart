fbeec56c0f554ec89f998840176f5168e2edecfe
liumin1128 <970568830@qq.com>
2019-08-24 22:49:46 +0800

    更新

--- lib/pages/user/me/index.dart
+++ lib/pages/user/me/index.dart
@@ -5,7 +5,6 @@ import 'package:reactmobi/blocs/user_bloc/index.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:reactmobi/components/Ui/Avatar/avatar.dart';
 
-
 class UserMe extends StatefulWidget {
   @override
   UserMeState createState() => UserMeState();
@@ -43,104 +42,39 @@ class UserMeState extends State<UserMe> {
     final themeBloc = BlocProvider.of<ThemeBloc>(context);
     final userBloc = BlocProvider.of<UserBloc>(context);
 
-    return BlocBuilder<UserBloc, UserState>(builder: (context, state) {
-      if (state is Uninitialized) {
-        return Center(child: CupertinoActivityIndicator());
-      }
-      if (state is Unauthenticated) {
-        return Center(child: Text('Unauthenticated'));
-      }
-      if(state is Authenticated) {
-        return CupertinoPageScaffold(
-            child: CustomScrollView(
-              controller: _scrollController,
-              physics: ScrollPhysics(),
-              slivers: <Widget>[
-                CupertinoSliverNavigationBar(
-                  largeTitle: Text('个人中心'),
-                  border: Border(top: BorderSide(style: BorderStyle.none)),
-                ),
-                SliverSafeArea(
-                  sliver: SliverToBoxAdapter(
-                    child: Column(children: <Widget>[
-                      Avatar(src: state.userInfo['avatarUrl'],),
-                      Text(state.userInfo['nickname'], style: CupertinoTheme.of(context).textTheme.navLargeTitleTextStyle),
-                    ],)
-                  ),
-                )
-              ],
-            )
-        );
-      }
- 
-
-    return CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
-        middle: Text("用户"),
-        border: Border(
-          top: BorderSide(
-            style: BorderStyle.none,
-          ),
-        ),
-        leading: GestureDetector(
-          onTap: () {
-            counterBloc.dispatch(CounterEvent.increment);
-          },
-          child: Icon(CupertinoIcons.bell),
-        ),
-        trailing: GestureDetector(
-          onTap: () {
-            userBloc.dispatch(LoginWithCode(context: context));
-          },
-          child: Icon(CupertinoIcons.bell),
-        ),
-        // trailing: Icon(CupertinoCupertinoIcons.add)
-        // backgroundColor: Colors.white,
-      ),
-      // child: Center(
-      //   child: Text('1111'),
-      // ),
-      child: Container(
-          child: Column(
-        children: <Widget>[
-          Center(
-            child: BlocBuilder<CounterBloc, int>(
-              builder: (context, count) {
-                return Center(
-                  child: Text(
-                    '$count',
-                    style: TextStyle(fontSize: 24.0),
-                  ),
-                );
-              },
-            ),
-          ),
-          Center(
-            child: BlocBuilder<UserBloc, UserState>(
-              builder: (context, state) {
-                return Center(
-                  child: Text(
-                    (state is Authenticated) ? state.token : 'no',
-                    style: TextStyle(fontSize: 24.0),
-                  ),
-                );
-              },
-            ),
-          ),
-          Center(
-            child: BlocBuilder<UserBloc, UserState>(
-              builder: (context, state) {
-                return Center(
-                  child: Text(
-                    (state is Authenticated) ? state.userInfo['nickname'] : 'no',
-                    style: TextStyle(fontSize: 24.0),
-                  ),
-                );
-              },
-            ),
-          )
-        ],
-      )),
+    return BlocBuilder<UserBloc, UserState>(
+      builder: (context, state) {
+        if (state is Uninitialized) {
+          return Center(child: CupertinoActivityIndicator());
+        } else if (state is Unauthenticated) {
+          return Center(child: Text('Unauthenticated'));
+        } else if (state is Authenticated) {
+          return CupertinoPageScaffold(
+              child: CustomScrollView(
+            controller: _scrollController,
+            physics: ScrollPhysics(),
+            slivers: <Widget>[
+              // CupertinoSliverNavigationBar(
+              //   largeTitle: Text('个人中心'),
+              //   border: Border(top: BorderSide(style: BorderStyle.none)),
+              // ),
+              SliverSafeArea(
+                sliver: SliverToBoxAdapter(
+                    child: Column(
+                  children: <Widget>[
+                    Avatar(
+                      src: state.userInfo['avatarUrl'],
+                    ),
+                    Text(state.userInfo['nickname'], style: CupertinoTheme.of(context).textTheme.navLargeTitleTextStyle),
+                  ],
+                )),
+              )
+            ],
+          ));
+        } else {
+          return Center(child: Text('Unauthenticated'));
+        }
+      },
     );
   }
 }
