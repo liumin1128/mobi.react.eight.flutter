21d9a04270f6a75e7fd496dab05fd0bc56091f41
liumin1128 <970568830@qq.com>
2019-08-24 08:25:12 +0800

    更新

--- lib/blocs/user_bloc/user_event.dart
+++ lib/blocs/user_bloc/user_event.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 
+@immutable
 abstract class UserEvent extends Equatable {
   UserEvent([List props = const []]) : super(props);
 }
