831353e96bd9d67ef1e0940a588bda5beb4f7de4
liumin1128 <970568830@qq.com>
2019-09-05 17:54:22 +0800

    优化样式

--- lib/utils/action.dart
+++ lib/utils/action.dart
@@ -21,22 +21,37 @@ Future alert({
         title: title == null ? null : Text(title),
         content: contentBuilder == null ? (content == null ? null : Text(content)) : contentBuilder,
         actions: <Widget>[
-          showCancel
-              ? CupertinoDialogAction(
-                  onPressed: () {
-                    Navigator.pop(context, Action.Cancel);
-                  },
-                  child: Text('取消'),
-                )
-              : null,
-          showConfirm
-              ? CupertinoDialogAction(
-                  onPressed: () {
-                    Navigator.pop(context, Action.Ok);
-                  },
-                  child: Text('确定'),
-                )
-              : null,
+          Container(
+            decoration: BoxDecoration(
+              border: Border(
+                right: BorderSide(color: Color(0xFFdddddd), width: 0.5),
+                top: BorderSide(color: Color(0xFFdddddd), width: 0.5),
+              ),
+            ),
+            child: showCancel
+                ? CupertinoDialogAction(
+                    onPressed: () {
+                      Navigator.pop(context, Action.Cancel);
+                    },
+                    child: Text('取消'),
+                  )
+                : null,
+          ),
+          Container(
+            decoration: BoxDecoration(
+              border: Border(
+                top: BorderSide(color: Color(0xFFdddddd), width: 0.5),
+              ),
+            ),
+            child: showConfirm
+                ? CupertinoDialogAction(
+                    onPressed: () {
+                      Navigator.pop(context, Action.Ok);
+                    },
+                    child: Text('确定'),
+                  )
+                : null,
+          )
         ],
       );
     },
