2873c1af91b5f8f3a12ce3155e5d85a531aedbab
liumin imac <liuminimac@liumindeiMac.lan>
2019-09-07 20:12:33 +0800

    gx1

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
-import 'dart:async';
 import 'package:flutter/cupertino.dart';
 import 'package:bloc/bloc.dart';
 import 'package:eight/utils/graphql.dart';
 import 'package:eight/blocs/delegate.dart';
 import 'app.dart';
 
-Future<void> main() async {
+main() {
   final client = getClient();
   BlocSupervisor.delegate = SimpleBlocDelegate();
   runApp(App(client: client));
