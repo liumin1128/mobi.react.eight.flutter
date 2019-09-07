b59399d5bc6da4eb8eb034e4a74f06c24a462928
liumin imac <liuminimac@liumindeiMac.lan>
2019-09-07 20:10:58 +0800

    gx1

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,12 @@
 import 'dart:async';
 import 'package:flutter/cupertino.dart';
 import 'package:bloc/bloc.dart';
-import 'app.dart';
 import 'package:eight/utils/graphql.dart';
-
-class SimpleBlocDelegate extends BlocDelegate {
-  @override
-  void onEvent(Bloc bloc, Object event) {
-    super.onEvent(bloc, event);
-    print(event);
-  }
-
-  @override
-  void onTransition(Bloc bloc, Transition transition) {
-    super.onTransition(bloc, transition);
-    print(transition);
-  }
-
-  @override
-  void onError(Bloc bloc, Object error, StackTrace stacktrace) {
-    super.onError(bloc, error, stacktrace);
-    print(error);
-  }
-}
+import 'package:eight/blocs/delegate.dart';
+import 'app.dart';
 
 Future<void> main() async {
   final client = getClient();
-
   BlocSupervisor.delegate = SimpleBlocDelegate();
-
   runApp(App(client: client));
 }
