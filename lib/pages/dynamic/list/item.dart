815d78db012b556b68743bfe1d1f7c26b27fdb21
liumin1128 <970568830@qq.com>
2019-10-06 16:08:49 +0800

    样式更新

--- lib/pages/dynamic/list/item.dart
+++ lib/pages/dynamic/list/item.dart
@@ -1,15 +1,30 @@
 import 'package:flutter/cupertino.dart';
 import 'package:eight/utils/common.dart';
 import 'package:eight/components/Avatar/index.dart';
-import 'package:eight/components/LikeBtn/index.dart';
+// import 'package:eight/components/LikeBtn/index.dart';
 import 'package:eight/components/MultiPicturesView/index.dart';
-import 'package:eight/utils/common.dart';
+// import 'package:eight/utils/common.dart';
+// import 'package:eight/components/Icons/Eva.dart';
+import 'package:eight/components/Icons/Taobao.dart';
 
 text2html(str) {
   return str;
   // .replaceAll(RegExp(r"(.*?)\[(.*?)_(.*?)]"), '\$1<img src="https://imgs.react.mobi/emoticon/\$2/\$3.gif" class="emoji" alt="[\$2_\$3]">');
   // .replace(/(.*?)\[(.*?)_(.*?)]/ig, )
   // .replace(/(.*?)\n(.*?)/ig, '$1<div>$2</div>');
+}
+
+actionButton(icon, text) {
+  return Container(
+    // padding: const EdgeInsets.only(right: 32),
+    child: Row(
+      children: <Widget>[
+        Icon(icon, color: Color(0xFF999999), size: 20),
+        Padding(padding: EdgeInsets.all(2)),
+        Text(text, style: TextStyle(fontSize: 16, color: Color(0xFF999999))),
+      ],
+    ),
+  );
 }
 
 class DynamicItem extends StatelessWidget {
@@ -19,7 +34,6 @@ class DynamicItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.symmetric(vertical: 16),
       decoration: BoxDecoration(color: CupertinoColors.white),
       margin: const EdgeInsets.only(bottom: 8),
       child: Column(
@@ -27,38 +41,44 @@ class DynamicItem extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           // 用户信息
-          Row(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 12),
-                child: Avatar(src: data.user.avatarUrl, size: 48),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    data.user.nickname,
-                    style: CupertinoTheme.of(context).textTheme.textStyle.copyWith(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Color(0xFF333333),
-                        ),
-                  ),
-                  Text(
-                    getTimeAgo(data.createdAt),
-                    style: CupertinoTheme.of(context).textTheme.textStyle.copyWith(
-                          fontSize: 12,
-                          color: Color(0xFF999999),
-                        ),
-                  ),
-                ],
-              )
-            ],
+
+          Container(
+            padding: EdgeInsets.only(top: 16),
+            child: Row(
+              children: <Widget>[
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 12),
+                  child: Avatar(src: data.user.avatarUrl, size: 48),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      data.user.nickname,
+                      style: CupertinoTheme.of(context).textTheme.textStyle.copyWith(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFF333333),
+                          ),
+                    ),
+                    Text(
+                      getTimeAgo(data.createdAt),
+                      style: CupertinoTheme.of(context).textTheme.textStyle.copyWith(
+                            fontSize: 14,
+                            // fontWeight: FontWeight.w300,
+                            color: Color(0xFFaaaaaa),
+                          ),
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
 
           Container(
-            padding: const EdgeInsets.only(left: 56),
+            padding: const EdgeInsets.only(left: 72, top: 8, right: 16, bottom: 8),
+            // color: Color(0xFF888888),
             child: Column(
               children: <Widget>[
                 // 文本部分
@@ -72,7 +92,7 @@ class DynamicItem extends StatelessWidget {
                         },
                         child: Container(
                           alignment: Alignment.topLeft,
-                          padding: const EdgeInsets.symmetric(horizontal: 16),
+                          // padding: const EdgeInsets.symmetric(horizontal: 16),
                           // color: Color(0xFF666666),
                           child: Text(
                             data.content,
@@ -91,10 +111,28 @@ class DynamicItem extends StatelessWidget {
                     ? Container(
                         // width: 300,
                         alignment: Alignment.topLeft,
-                        padding: const EdgeInsets.only(top: 14, left: 14, right: 14),
+                        padding: const EdgeInsets.only(top: 16),
                         child: multiPictureView(data.pictures),
                       )
                     : Container(),
+
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 16),
+                  child: Row(
+                    // alignment: Alignment.centerLeft,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    // padding: EdgeInsets.only(top: 16, left: 16, right: 16, bottom: 16),
+                    children: <Widget>[
+                      actionButton(TaobaoIcons.forward, '233'),
+                      actionButton(TaobaoIcons.comment, '233'),
+                      actionButton(TaobaoIcons.appreciate, '233'),
+                      actionButton(TaobaoIcons.share, '分享'),
+                    ],
+                  ),
+                )
+
+                // 用户操作
               ],
             ),
           ),
